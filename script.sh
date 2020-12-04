7349d6e6449e4d4dbad974be41c626a98deb10bb
jason-huling <jason-huling@users.noreply.github.com>
2020-12-04 02:20:10 -0800

    Running raw tflint

--- script.sh
+++ script.sh
@@ -9,17 +9,20 @@ echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/review
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo '::endgroup::'
 
-echo '::group:: Installing tflint v0.20.3 ... https://github.com/terraform-linters/tflint'
-curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/tags/v0.20.3 | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip && unzip tflint.zip -d "${TEMP_PATH}" && rm tflint.zip
+echo '::group:: Installing tflint ... https://github.com/terraform-linters/tflint'
+curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip && unzip tflint.zip -d "${TEMP_PATH}" && rm tflint.zip
 echo '::endgroup::'
 
-echo INPUT_FLAGS="${INPUT_FLAGS}"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+echo '::group:: Running raw tflint ...'
+tflint ${INPUT_FLAGS} .
+echo '::endgroup::'
+
 echo '::group:: Running tflint with reviewdog 🐶 ...'
 # shellcheck disable=SC2086
-tflint --format=checkstyle --enable-rule=terraform_documented_variables --enable-rule=terraform_comment_syntax . \
+tflint --format=checkstyle ${INPUT_FLAGS} . \
   | reviewdog -f=checkstyle -name="tflint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
 
 tflint_return="${PIPESTATUS[0]}" reviewdog_return="${PIPESTATUS[1]}" exit_code=$?
